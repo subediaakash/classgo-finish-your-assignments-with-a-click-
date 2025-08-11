675d67323e769aef92571231fd485138be691bb2
spartan-readytocode <7007966033abc@gmail.com>
2025-08-11 22:41:24 +0530

    feat : done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inconsolata } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,11 +9,6 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inconsolata = Inconsolata({
-  variable: "--font-inconsolata",
   subsets: ["latin"],
 });
 
@@ -30,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inconsolata.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6 ">
-      <h1 className="text-4xl font-bold font-inconsolata">Everything is Possible Untill You Never Give Up</h1>
-      <p className="font-bold text-2xl font-inconsolata">DETOX IT</p>
+      <h1 className="text-4xl font-bold">Everything is Possible Untill You Never Give Up</h1>
+      <p className="font-bold text-2xl">DETOX IT</p>
     </div>
   );
 }
