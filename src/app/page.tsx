781d7c12640c40c4bcea4fbc3fb53080ed3ee0ab
spartan-readytocode <7007966033abc@gmail.com>
2025-08-11 22:39:44 +0530

    feat : done something

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-6 ">
+      <h1 className="text-4xl font-bold font-inconsolata">Everything is Possible Untill You Never Give Up</h1>
+      <p className="font-bold text-2xl font-inconsolata">DETOX IT</p>
+    </div>
+  );
+}
